90eeb692c04006982d4e5cc06d8fed193c09ae92
masudhossaincse <csemasud808@gmail.com>
2020-03-25 22:01:51 +0600

    pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,19 +3,23 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
-        <input [value]="email" (keyup.enter)="email= $event.target.value; onKeyUp()" />   
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />   
-
+           {{ course.title | lowercase }} <br/>
+           {{ course.students | number }} <br/>
+           {{ course.rating |number:'1.2-2' }} <br/>
+           {{ course.price | currency:'BDT  ':true }} <br/>
+           {{ course.releaseDate | date:shortDate }} <br/>
+ 
     `
 })
 
 export class CoursesComponent {
-    title = "List of Course ";
-    email="rana@eatlbd.com";
-       onKeyUp() {
-         console.log(this.email);
-       }
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.9,
+        students: 567567,
+        price: 5678,
+        releaseDate: new Date(2020, 3, 3) 
+    }
 
 
 
