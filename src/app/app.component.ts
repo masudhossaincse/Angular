1af7208ac7789855111d8d9ce980132068bd080d
masudhossaincse <csemasud808@gmail.com>
2020-03-26 20:52:58 +0600

    Component API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'TestingAngular';
   heading = 'Angular';
+  post = {
+    title: "Title",
+    isFavorite: true
+  }
 }
